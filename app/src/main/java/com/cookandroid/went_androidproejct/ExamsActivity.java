afd33773c9cf0e3c3cc184bf0cb601ae8065d4b8
suyeon kim <88700545+sueyon032@users.noreply.github.com>
2023-09-05 19:54:18 +0900

    exam화면 구현중

---
+++ app/src/main/java/com/cookandroid/went_androidproejct/ExamsActivity.java
@@ -0,0 +1,13 @@
+package com.cookandroid.went_androidproejct;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExamsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exams);
+    }
+}
