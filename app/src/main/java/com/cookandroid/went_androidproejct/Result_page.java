442188d465dd897ca3cf7483793b739ad97d5080
seoyeon0203 <w2222@e-mirim.hs.kr>
2023-09-07 18:43:11 +0900

    result page 생성

---
+++ app/src/main/java/com/cookandroid/went_androidproejct/Result_page.java
@@ -0,0 +1,16 @@
+package com.cookandroid.went_androidproejct;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Result_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_page);
+    }
+
+
+}
